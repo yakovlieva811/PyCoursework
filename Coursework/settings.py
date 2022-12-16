93ea238468006a2c7d127ca1e0406f997fd9c061
Mariia Y <yakovlieva811>
2022-12-17 01:19:10 +0200

    pythonanywhere fix

--- Coursework/settings.py
+++ Coursework/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Coursework.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['Templates'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
